b2d51e6c9239dd22b7a1a199e6b0b911bbfd9c2c
Marcelo Braga <b3darcademy@outlook.com>
2022-07-16 02:49:25 -0300

    useState

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,5 +1,5 @@
-import buttonIcon from '../../assets/img/notification-icon.svg'
-import './styles.css'
+import buttonIcon from '../../assets/img/notification-icon.svg';
+import './styles.css';
 
 function NotificationButton() {
     return (
@@ -9,4 +9,4 @@ function NotificationButton() {
     )
 }
 
-export default NotificationButton
+export default NotificationButton;
