83498e7172a83be2a116ca3575793e9b23ed5d0c
Marcelo Braga <b3darcademy@outlook.com>
2022-07-26 01:02:41 -0300

    commit for error fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+// Linha para resolver o erro no SMS na aplicação do Netlify (tela preta / erro):
 window.React = React
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
